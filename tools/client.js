5aebdea2ca01135ddee36b684cfc791ed8a1a14f
ash <ash@strategicdata.com.au>
2020-07-15 15:54:19 +1000

    tidy up client testing code

--- tools/client.js
+++ tools/client.js
@@ -1,9 +1,15 @@
 const axios = require('axios');
+
+axios.post('http://localhost:3000/create', {})
+     .then(response => console.log(response.data))
+     .catch(error => console.log("Something went wrong..", error.response.data));
+
+const shortid = 'eQ2fuX_jQt';
 const data  = {
-  'organisation.name': 'not digitalx',
+  'organisation.name': 3500,
   'organisation.postcode': 3333,
 };
 
-axios.post('http://localhost:3000/save/1', data).then(response => {
-  console.log(response.data);
-});
+axios.post(`http://localhost:3000/save/${ shortid }`, data)
+     .then(response => console.log(response.data))
+     .catch(error => console.log("Something went wrong..", error.response.data));
