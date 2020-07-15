8d403108ad660f16ca897d0f60edbab644c9fbd5
ash <ash@strategicdata.com.au>
2020-07-16 08:44:30 +1000

    Add getter test

--- tools/client.js
+++ tools/client.js
@@ -4,11 +4,15 @@ axios.post('http://localhost:3000/create', {})
      .then(response => console.log(response.data))
      .catch(error => console.log("Something went wrong..", error.response.data));
 
-const shortid = 'eQ2fuX_jQt';
+const shortid = 'P6qQwI1U6';
 const data  = {
-  'organisation.name': 3500,
+  'organisation.name': "i am string",
   'organisation.postcode': 3333,
 };
+
+axios.get(`http://localhost:3000/organisation/${ shortid }`, { params: { fields: ['name', 'something-else'] } })
+     .then(response => console.log(response.data))
+     .catch(error => console.log("Something went wrong..", error.response.data));
 
 axios.post(`http://localhost:3000/save/${ shortid }`, data)
      .then(response => console.log(response.data))
