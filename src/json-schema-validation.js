b8bf9a5ce86412a2f657527efcbccbe9e5e48a5a
ash <ash@strategicdata.com.au>
2020-07-15 15:54:07 +1000

    var -> const

--- src/json-schema-validation.js
+++ src/json-schema-validation.js
@@ -1,8 +1,8 @@
-var fs = require('fs');
-var yaml = require('yaml');
+const fs = require('fs');
+const yaml = require('yaml');
 
-var Ajv = require('ajv');
-var ajv = new Ajv({allErrors: true});
+const Ajv = require('ajv');
+const ajv = new Ajv({allErrors: true});
 
 const file = fs.readFileSync('./schema.yaml', 'utf8')
 const data = yaml.parse(file)
@@ -20,7 +20,7 @@ for (table of tables) {
 
 
 // JSON schema validator
-var schema   = { properties };
-var validate = ajv.compile(schema);
+let schema   = { properties };
+let validate = ajv.compile(schema);
 
 module.exports = validate;
