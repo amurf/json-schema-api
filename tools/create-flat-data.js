f39b655dce577d9e99ffea2d4aea6414904695d5
ash <ash@strategicdata.com.au>
2020-07-19 21:23:36 +1000

    Tidy up export script

--- tools/create-flat-data.js
+++ tools/create-flat-data.js
@@ -2,17 +2,58 @@ var fs = require('fs');
 var yaml = require('yaml');
 
 var pg = require('knex')({
-    client: 'pg',
-    connection: process.env.DB_URI,
-    searchPath: ['knex', 'public'],
+  client: 'pg',
+  connection: process.env.DB_URI,
+  searchPath: ['knex', 'public'],
 });
 
 const { data } = require('../src/schema.js');
 
+let exportPromises = [];
 for (let table of Object.keys(data)) {
-  let columns = data[table];
+  let columns = Object.keys(data[table]).map(columnName => {
+    return { name: columnName, ...data[table][columnName] }
+  });
+  const exportTableName = `${ table }_export`
 
-  pg.schema.createTable(table + "_export", function (table) {
+  exportPromises.push(doExport(table, exportTableName, columns));
+}
+
+Promise.all(exportPromises)
+       .then(ok => console.log("Databases exported"))
+       .catch(error => console.log("Something went wrong", error))
+       .finally(_ => pg.destroy());
+
+async function doExport(tableName, exportTableName, columns) {
+  await createTable(tableName, exportTableName, columns);
+  await exportTable(tableName, exportTableName, columns);
+}
+
+async function exportTable(tableName, exportTableName, columns) {
+  const columnNames     = columns.map(column => column.name);
+  const columnSelectors = columns.map(column => {
+    let jsonRead =  `data->>'${ column.name }'`;
+    if (column.type == 'number') {
+      jsonRead =  `(data->>'${ column.name }')::int`;
+    }
+    let jsonField = `${ jsonRead } as ${ column.name }`;
+    return pg.raw(jsonField);
+  });
+
+  let subquery = pg(tableName).select(columnSelectors);
+  let insert   = await pg(
+    pg.raw("?? (" + columnNames.map(_ => '??').join(',') + ")",
+      [exportTableName, ...columnNames])
+  ).insert(subquery)
+}
+
+async function createTable(tableName, exportTableName, columns) {
+  const exists = await pg.schema.hasTable(exportTableName);
+  if (exists) {
+    return;
+  }
+
+  let response = await pg.schema.createTable(exportTableName, table => {
     table.increments();
     columns.map(column => {
       if (column.type == 'number') {
@@ -21,22 +62,7 @@ for (let table of Object.keys(data)) {
         table[column.type](column.name);
       }
     });
-  }).then(row => {
-
-    const columnNames = columns.map(column => column.name);
-    const columnSelectors = columns.map(column => {
-      let jsonRead =  `data->>'${ column.name }'`; 
-      if (column.type == 'number') {
-        jsonRead =  `(data->>'${ column.name }')::int`;
-      }
-      let jsonField = `${ jsonRead } as ${ column.name }`;
-      return pg.raw(jsonField);
-    });
-    let subquery = pg(table).select(columnSelectors);
-
-    let insert = pg(
-      pg.raw("?? (" + columnNames.map(_ => '??').join(',') + ")",
-        [table + "_export", ...columnNames])
-    ).insert(subquery).then(row => console.log(row));
   });
+
+  return response;
 }
